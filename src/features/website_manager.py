c28f414927448fa8ce6b968ecba501974e69f496
Robert Meissner <robert.meissner@codecentric.de>
2020-12-09 16:25:41 +0100

    - Connecting to dedicated splash container

--- src/features/website_manager.py
+++ src/features/website_manager.py
@@ -54,7 +54,7 @@ class Singleton:
 class WebsiteManager:
     website_data: WebsiteData
 
-    SPLASH_URL = "http://localhost:8050"
+    SPLASH_URL = "http://splash:8050"
     SPLASH_HEADERS = {
         "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) "
         "AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.163 Safari/537.36"
