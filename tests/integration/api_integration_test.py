abeff8cc5c9a854b8e9d76f53fe2c0eaa99e9ce6
Robert Meissner <robert.meissner@codecentric.de>
2021-02-01 13:11:02 +0100

    - Ping unit test

--- tests/integration/api_integration_test.py
+++ tests/integration/api_integration_test.py
@@ -1,34 +1,48 @@
 import json
+import multiprocessing
 import os
+import time
 
-import pytest
 import requests
+import uvicorn
+
+from app.api import app
+from app.communication import ProcessToDaemonCommunication
+from lib.settings import API_PORT
 
 if "PRE_COMMIT" in os.environ:
-    from test_libs import (
-        DOCKER_TEST_HEADERS,
-        DOCKER_TEST_URL,
-        _build_and_run_docker,
-    )
+    from test_libs import DOCKER_TEST_HEADERS, DOCKER_TEST_URL
 else:
-    from tests.test_libs import (
-        DOCKER_TEST_HEADERS,
-        DOCKER_TEST_URL,
-        _build_and_run_docker,
-    )
+    from tests.test_libs import DOCKER_TEST_HEADERS, DOCKER_TEST_URL
+
+"""
+--------------------------------------------------------------------------------
+"""
 
 
-@pytest.mark.skip(
-    reason="This test takes a lot of time, depending on payload etc. Execute manually."
-)
+def _start_api(queue, return_queue):
+    app.api_queue = ProcessToDaemonCommunication(queue, return_queue)
+    uvicorn.run(app, host="0.0.0.0", port=API_PORT, log_level="info")
+
+
 def test_ping_container():
-    url = DOCKER_TEST_URL + "_ping"
-
-    _build_and_run_docker()
+    api_to_manager_queue = multiprocessing.Queue()
+    manager_to_api_queue = multiprocessing.Queue()
+    api_process = multiprocessing.Process(
+        target=_start_api,
+        args=(api_to_manager_queue, manager_to_api_queue),
+    )
+    api_process.start()
+    time.sleep(0.1)
 
     response = requests.request(
-        "GET", url, headers=DOCKER_TEST_HEADERS, timeout=60
+        "GET",
+        DOCKER_TEST_URL + "_ping",
+        headers=DOCKER_TEST_HEADERS,
+        timeout=60,
     )
+    api_process.terminate()
+    api_process.join()
 
     data = json.loads(response.text)
     is_ok = data["status"] == "ok"
