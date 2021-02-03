a62009853092d0a857d993dbcfcb6abc425b3215
Robert Meissner <robert.meissner@codecentric.de>
2021-02-03 17:20:15 +0100

    - Adding wily config

--- src/tools/dev/wily.sh
+++ src/tools/dev/wily.sh
@@ -3,6 +3,8 @@ poetry run wily build src
 poetry run wily rank --asc -l 4 | grep ".py"
 
 poetry run wily graph src/features/metadata_base.py mi
-poetry run wily graph src/features/gdpr.py mi
-poetry run wily graph src/features/extract_from_files.py mi
-poetry run wily graph src/features/website_manager.py mi
+poetry run wily graph src/features/metadata_base.py raw.sloc
+poetry run wily graph src/features/metadata_base.py cyclomatic.complexity
+# poetry run wily graph src/features/gdpr.py mi
+# poetry run wily graph src/features/extract_from_files.py mi
+# poetry run wily graph src/features/website_manager.py mi
