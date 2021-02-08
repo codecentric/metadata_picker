db72be784501ca9f30f03844a64516f60f5751c1
Robert Meissner <robert.meissner@codecentric.de>
2021-02-08 15:38:48 +0100

    - Refactoring security.py to reduce complexity

--- src/features/security.py
+++ src/features/security.py
@@ -29,42 +29,58 @@ class Security(MetadataBase):
 
         for tag, expected_value in self.tags.items():
             if tag in website_data.headers:
-                header_value = [
-                    self._work_text(value).replace(",", ";").split(";")
-                    for value in website_data.headers[tag]
-                ]
-                header_value = [el for val in header_value for el in val]
-
-                for idx, element in expected_value.items():
-                    expected_value.update(
-                        {
-                            int(idx): [
-                                self._work_text(value) for value in element
-                            ]
-                        }
-                    )
-
-                found_values = sum(
-                    [
-                        1
-                        for value in expected_value.values()
-                        for val in value
-                        if val in header_value
-                    ]
+                header_value = self._extract_header_values(
+                    website_data.headers[tag]
                 )
 
-                if tag == "strict-transport-security":
-                    for el in header_value:
-                        if (
-                            el.startswith("maxage=")
-                            and int(el.split("=")[-1]) > 0
-                        ):
-                            found_values += 1
+                expected_value = self._process_expected_values(expected_value)
+
+                found_values = self._number_of_expected_keys_in_header(
+                    expected_value, header_value
+                )
+
+                if (
+                    tag == "strict-transport-security"
+                    and self._is_sts_mag_age_greater_than_zero(header_value)
+                ):
+                    found_values += 1
 
                 if found_values == len(expected_value.keys()):
                     values.append(tag)
 
         return {VALUES: values}
+
+    def _process_expected_values(self, expected_value: dict) -> dict:
+        for idx, element in expected_value.items():
+            expected_value.update(
+                {int(idx): [self._work_text(value) for value in element]}
+            )
+        return expected_value
+
+    def _extract_header_values(self, header: list) -> list:
+        header_value = [
+            self._work_text(value).replace(",", ";").split(";")
+            for value in header
+        ]
+        return [el for val in header_value for el in val]
+
+    def _is_sts_mag_age_greater_than_zero(self, header_value: list) -> bool:
+        greater_than_zero = False
+        for el in header_value:
+            if el.startswith("maxage=") and int(el.split("=")[-1]) > 0:
+                greater_than_zero = True
+        return greater_than_zero
+
+    def _number_of_expected_keys_in_header(self, expected_value, header_value):
+        found_values = sum(
+            [
+                1
+                for value in expected_value.values()
+                for val in value
+                if val in header_value
+            ]
+        )
+        return found_values
 
     def _decide(self, website_data: WebsiteData) -> tuple[bool, float]:
         probability = len(website_data.values) / len(self.tags.keys())
